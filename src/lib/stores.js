866a78dad71052f25a9cb0bc6925013ea85f79cf
HexiDev <yourioldenzeel08@gmail.com>
2023-12-18 09:05:06 +0100

    Add preloadData function to page transition in stores.js

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -1,10 +1,11 @@
-import { goto } from "$app/navigation";
+import { goto, preloadData } from "$app/navigation";
 import { writable } from "svelte/store";
 
 const transitionPage = writable(false);
 const pageTransition = writable({
   goto: async (link) => {
     transitionPage.set(true);
+    preloadData(link);
     // wait 400ms
     await new Promise((r) => setTimeout(r, 890));
     goto(link);
